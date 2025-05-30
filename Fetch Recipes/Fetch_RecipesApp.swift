7310dee8ab3fba87943ce912a6a4e807f2624ade
Pranay Kothlapuram <pranay.kothlapuram@angi.com>
2025-05-30 10:53:38 -0500

    Initial commit

---
+++ Fetch Recipes/Fetch_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_RecipesApp.swift
+//  Fetch Recipes
+//
+//  Created by Pranay Kothlapuram on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
